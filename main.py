af18eaebe7e30a3d40b2d9f6c270fda5fe1a8c4c
sammyamajumdar <sammya512@gmail.com>
2025-08-28 17:42:25 +0100

    added new function to test CI pipeline

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def main(): 
     return "hello world"
 
+def add_two_numbers(a, b): 
+    return a + b 
+
 if __name__=="__main__": 
+    x = add_two_numbers(5,5)
+    print(x)
     main()
--- test_main.py
+++ test_main.py
@@ -1,4 +1,7 @@
-from main import main
+from main import main, add_two_numbers
 
 def test_main():
     assert main() == "hello world"
+
+def test_add_two_numbers(): 
+    assert add_two_numbers(6,10) == 16
